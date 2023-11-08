60989ebf339852f0ce3ac8d6c1d34e713a4bf473
DenisFeofanov <omail4me@gmail.com>
2023-11-08 13:49:37 +0500

    [feat]: add feedback form validation

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,17 +1,40 @@
-import Layout from "@/pages/Layout";
 import AppLink from "@/components/AppLink";
 import Heading from "@/components/Heading";
-import { useRouter } from "next/router";
-import { FormEvent } from "react";
+import Layout from "@/pages/Layout";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+interface FormData {
+  fullName: string;
+  email: string;
+}
+
+const schema = yup
+  .object({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+  })
+  .required();
 
 export default function Feedback() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
   const router = useRouter();
 
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
+  const onSubmit = (data: FormData) => {
+    console.log(data);
     router.push("/");
-  }
+  };
+
   return (
     <>
       <Head>
@@ -22,11 +45,21 @@ export default function Feedback() {
         <Heading>Обратная связь</Heading>
 
         <form
-          onSubmit={e => handleSubmit(e)}
+          onSubmit={handleSubmit(data => onSubmit(data))}
           className="flex flex-col gap-3 max-w-sm"
         >
-          <input className="border" type="text" />
-          <input className="border" type="text" />
+          <input
+            {...register("fullName", { required: true })}
+            className="border"
+            type="text"
+          />
+          {errors.fullName && <p>ФИО обязательно</p>}
+          <input
+            {...register("email", { required: true })}
+            className="border"
+            type="text"
+          />
+          {errors.email && <p>Введите корректный email</p>}
 
           <button type="submit">Отправить</button>
         </form>
