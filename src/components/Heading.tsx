1ae385071099df65ba7f7acd4c36f5565aa05d38
DenisFeofanov <omail4me@gmail.com>
2023-11-17 13:42:11 +0500

    Revert "[test] test preview branch"
    This reverts commit a43b061392f435bd5c87b32544c3df83ef267147.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 function Heading({ children }: Props) {
-  return <h1 className="m-4 text-3xl text-red-800">{children}</h1>;
+  return <h1 className="m-4 text-3xl">{children}</h1>;
 }
 
 export default Heading;
