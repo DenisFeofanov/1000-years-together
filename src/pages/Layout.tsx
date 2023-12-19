73c690184601d814a7591e455f58c5b04d72e592
DenisFeofanov <omail4me@gmail.com>
2023-12-19 23:55:57 +0500

    [fix] set header button/title

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -10,13 +10,24 @@ export default function Layout({ children }: Props) {
   const currPathname = usePathname();
   const pagesWithCustomFooter =
     currPathname === "/choose-stories" || currPathname?.split("/")[1] === "act";
-  let headerTitle,
-    titleType: TitleType = "link";
-  if (currPathname === "/") {
-    headerTitle = "Бессмертие";
-    titleType = "text";
-  } else {
-    headerTitle = "тысяча лет вместе";
+  let headerTitle, titleType: TitleType;
+
+  switch (currPathname) {
+    case "/":
+      headerTitle = "Бессмертие";
+      titleType = "text";
+      break;
+    case "/about":
+    case "/feedback":
+    case "/tutorial":
+    case "/privacy":
+      headerTitle = "тысяча лет вместе";
+      titleType = "link";
+      break;
+    default:
+      headerTitle = "тысяча лет вместе";
+      titleType = "text";
+      break;
   }
 
   return (
