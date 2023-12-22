820462c1094d38e4ffbec6109c557d63fe4fdc2a
DenisFeofanov <omail4me@gmail.com>
2023-12-22 19:13:38 +0500

    [feat] add hover and active for start button

--- src/components/ChooseStoriesLink/index.tsx
+++ src/components/ChooseStoriesLink/index.tsx
@@ -72,12 +72,12 @@ function ChooseStoriesLink({
       <div
         className={`${
           styles.container
-        } mt-[80px] h-[300px] border-t border-t-grayDark flex justify-center items-center md:px-[12px] md:fixed md:bottom-0 md:left-0 md:right-0 md:grid md:grid-cols-[minmax(0,1fr),minmax(0,1fr),minmax(0,1fr)] ${
+        } mt-[80px] h-[300px] border-t border-t-grayDark text-blackText flex justify-center items-center md:px-[12px] md:fixed md:bottom-0 md:left-0 md:right-0 md:grid md:grid-cols-[minmax(0,1fr),minmax(0,1fr),minmax(0,1fr)] ${
           isSelectingDone ? "bg-greenSoft" : "bg-grayNum md:bg-white"
-        }`}
+        } fine-pointer:hover:bg-grayDark fine-pointer:hover:text-white active:bg-grayDark active:text-white`}
       >
         <div className="lg:relative self-end">
-          <p className="hidden md:block md:uppercase md:text-blackText md:text-[1rem] md:not-italic md:font-semibold md:leading-[normal] md:pb-[14px]">
+          <p className="hidden md:block md:uppercase md:text-[1rem] md:not-italic md:font-semibold md:leading-[normal] md:pb-[14px]">
             {selectedStoriesText}
           </p>
 
@@ -95,7 +95,7 @@ function ChooseStoriesLink({
         <button
           className={`${
             styles.container__title
-          } whitespace-nowrap leading-[normal] py-1 px-2 text-grayDark text-[1.25rem] xsm:text-[1.5rem] font-semibold border-2 border-transparent uppercase flex gap-2 justify-center items-center before:content-["("] before:font-normal before:text-[1.33em] after:content-[")"] after:font-normal after:text-[1.33em] max-md:fine-pointer:hover:bg-grayDark max-md:fine-pointer:hover:text-white max-md:fine-pointer:hover:border-2 max-md:fine-pointer:hover:border-grayDark max-md:active:bg-grayDark max-md:active:text-white max-md:active:border-2 max-md:active:border-grayDark md:text-[1rem] ${
+          } whitespace-nowrap leading-[normal] py-1 px-2 text-[1.25rem] xsm:text-[1.5rem] font-semibold border-2 border-transparent uppercase flex gap-2 justify-center items-center before:content-["("] before:font-normal before:text-[1.33em] after:content-[")"] after:font-normal after:text-[1.33em] max-md:fine-pointer:hover:bg-grayDark max-md:fine-pointer:hover:text-white max-md:fine-pointer:hover:border-2 max-md:fine-pointer:hover:border-grayDark max-md:active:bg-grayDark max-md:active:text-white max-md:active:border-2 max-md:active:border-grayDark md:text-[1rem] ${
             isDisabled && "pointer-events-none cursor-default text-grayMiddle"
           }`}
           type="button"
