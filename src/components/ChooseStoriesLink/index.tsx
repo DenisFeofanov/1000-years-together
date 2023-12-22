f1b69c3bde55d28ee486a83d87a16d58bac7b3df
DenisFeofanov <omail4me@gmail.com>
2023-12-22 17:51:47 +0500

    [feat] start button wrap in link and add animation

--- src/components/ChooseStoriesLink/index.tsx
+++ src/components/ChooseStoriesLink/index.tsx
@@ -17,23 +17,20 @@ function ChooseStoriesLink({
   storiesTooltip,
   ...rest
 }: Props) {
-  const linkRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const handleScroll = () => {
-      if (linkRef.current) {
-        const buttonHeight = linkRef.current.offsetHeight;
+      const PX_FOR_ANIMATION = 450;
 
-        const scrollStart =
-          window.scrollY -
-          (document.body.offsetHeight - window.innerHeight - buttonHeight);
+      const scrollStart =
+        window.scrollY -
+        (document.body.offsetHeight - window.innerHeight - PX_FOR_ANIMATION);
 
-        const scrollProportion = scrollStart / buttonHeight;
+      const scrollProportion = scrollStart / PX_FOR_ANIMATION;
 
-        document.body.style.setProperty(
-          "--animated-btn-scroll",
-          scrollProportion > 0 ? String(scrollProportion) : "0"
-        );
-      }
+      document.body.style.setProperty(
+        "--animated-btn-scroll",
+        scrollProportion > 0 ? String(scrollProportion) : "0"
+      );
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -42,20 +39,29 @@ function ChooseStoriesLink({
   }, []);
 
   const isDisabled = !isSelectingDone;
-  const disabledStyles =
-    isDisabled && "pointer-events-none cursor-default text-grayMiddle";
 
   return (
-    <>
+    <Link
+      className={`${
+        isDisabled && "pointer-events-none cursor-default text-grayMiddle"
+      }`}
+      href={href}
+      // onClick check keeps link focusable for accessibility, keeps prefetch, but prevents going further
+      onClick={e => isDisabled && e.preventDefault()}
+      aria-disabled={isDisabled}
+      {...rest}
+    >
       <div className="hidden md:block md:h-[calc(var(--animated-btn-height)+119px)]"></div>
+
       <div
-        className={`mt-[80px] h-[300px] border-t border-t-grayDark flex justify-center items-center md:h-[var(--animated-btn-height)] md:px-[12px] md:py-[7px] md:mt-[0] md:fixed md:bottom-0 md:left-0 md:right-0 md:grid md:grid-cols-3 ${
+        className={`${
+          styles.animatedBtn
+        } mt-[80px] h-[300px] border-t border-t-grayDark flex justify-center items-center md:h-[50px] md:px-[12px] md:fixed md:bottom-0 md:left-0 md:right-0 md:grid md:grid-cols-[minmax(0,1fr),minmax(0,1fr),minmax(0,1fr)] ${
           isSelectingDone ? "bg-greenSoft" : "bg-grayNum md:bg-white"
         }`}
-        ref={linkRef}
       >
-        <div className="lg:relative">
-          <p className="hidden md:block md:uppercase md:text-blackText md:text-[1rem] md:not-italic md:font-semibold md:leading-[normal]">
+        <div className="lg:relative self-end">
+          <p className="hidden md:block md:uppercase md:text-blackText md:text-[1rem] md:not-italic md:font-semibold md:leading-[normal] md:pb-[14px]">
             {selectedStoriesText}
           </p>
 
@@ -70,18 +76,19 @@ function ChooseStoriesLink({
           </p>
         </div>
 
-        <Link
-          className={`w-full h-full leading-[normal] py-1 px-2 text-grayDark text-[1.25rem] xsm:text-[1.5rem] font-semibold border-2 border-transparent uppercase flex gap-2 justify-center items-center before:content-["("] before:font-normal before:text-[1.33em] after:content-[")"] after:font-normal after:text-[1.33em] max-md:fine-pointer:hover:bg-grayDark max-md:fine-pointer:hover:text-white max-md:fine-pointer:hover:border-2 max-md:fine-pointer:hover:border-grayDark max-md:active:bg-grayDark max-md:active:text-white max-md:active:border-2 max-md:active:border-grayDark md:text-[1rem] ${disabledStyles}`}
-          href={href}
-          // onClick check keeps link focusable for accessibility, keeps prefetch, but prevents going further
-          onClick={e => isDisabled && e.preventDefault()}
-          aria-disabled={isDisabled}
-          {...rest}
+        <button
+          className={`${
+            styles.animatedBtnTitle
+          } whitespace-nowrap leading-[normal] py-1 px-2 text-grayDark text-[1.25rem] xsm:text-[1.5rem] font-semibold border-2 border-transparent uppercase flex gap-2 justify-center items-center before:content-["("] before:font-normal before:text-[1.33em] after:content-[")"] after:font-normal after:text-[1.33em] max-md:fine-pointer:hover:bg-grayDark max-md:fine-pointer:hover:text-white max-md:fine-pointer:hover:border-2 max-md:fine-pointer:hover:border-grayDark max-md:active:bg-grayDark max-md:active:text-white max-md:active:border-2 max-md:active:border-grayDark md:text-[1rem] ${
+            isDisabled && "pointer-events-none cursor-default text-grayMiddle"
+          }`}
+          type="button"
+          disabled={isDisabled}
         >
           Начать спектакль
-        </Link>
+        </button>
       </div>
-    </>
+    </Link>
   );
 }
 
