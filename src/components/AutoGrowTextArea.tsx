39454d3c2cc8cd642cb6a1a1c36a553bb758bbe7
DenisFeofanov <omail4me@gmail.com>
2023-12-25 20:09:33 +0500

    [feat] style autogrowing textarea

--- src/components/AutoGrowTextArea.tsx
+++ src/components/AutoGrowTextArea.tsx
@@ -6,7 +6,6 @@ import { ChangeEvent, forwardRef, useState } from "react";
 import { Path, UseFormRegister } from "react-hook-form";
 
 interface Props {
-  className?: string;
   label: Path<FormData>;
   placeholder: string;
 }
@@ -15,7 +14,7 @@ const AutoGrowTextArea = forwardRef<
   HTMLTextAreaElement,
   Props & ReturnType<UseFormRegister<FormData>>
 >(function AutoGrowTextArea(
-  { className, label, placeholder, onBlur, onChange, name },
+  { label, placeholder, onBlur, onChange, name },
   ref
 ) {
   const [value, setValue] = useState("");
@@ -25,13 +24,19 @@ const AutoGrowTextArea = forwardRef<
     onChange(e);
   }
 
+  const inputStyles =
+    "py-[20px] min-h-[71px] text-grayDark text-[1.25rem] xsm:text-[1.25rem] not-italic font-semibold leading-[1.1] tracking-[0.24px] placeholder:text-grayMiddle placeholder:text-[1.75rem] placeholder:not-italic placeholder:font-semibold placeholder:leading-[1.1] placeholder:tracking-[-0.28px] border-b-2 border-b-grayMiddle md:min-h-[87px] md:placeholder:text-[2.25rem] md:placeholder:leading-[1.3] md:placeholder:tracking-[-0.0225rem] md:text-[1.5rem] md:leading-[1.3] md:tracking-[-0.0225rem] focus:border-grayDark";
+
   return (
     <div className="grid">
-      <span className="row-start-1 col-start-1 row-end-2 col-end-2 whitespace-pre-wrap invisible">
+      <span
+        className={`row-start-1 col-start-1 row-end-2 col-end-2 whitespace-pre-wrap invisible ${inputStyles}`}
+      >
         {value}
       </span>
+
       <textarea
-        className={`resize-none overflow-hidden row-start-1 col-start-1 row-end-2 col-end-2 ${className}`}
+        className={`resize-none overflow-hidden row-start-1 col-start-1 row-end-2 col-end-2 ${inputStyles}`}
         onChange={handleChange}
         {...{ ref, onBlur, name, placeholder }}
       >
--- src/components/FeedbackInput.tsx
+++ src/components/FeedbackInput.tsx
@@ -18,7 +18,7 @@ const FeedbackInput = ({
 }: InputProps) => (
   <>
     <input
-      className="py-[20px] h-[71px] text-grayDark text-[1.25rem] xsm:text-[1.5rem] not-italic font-semibold leading-[1.1] tracking-[0.24px] placeholder:text-grayMiddle placeholder:text-[1.75rem] placeholder:not-italic placeholder:font-semibold placeholder:leading-[1.1] placeholder:tracking-[-0.28px] border-b-2 border-b-grayMiddle md:h-[87px] md:placeholder:text-[2.25rem] md:placeholder:leading-[1.3] md:placeholder:tracking-[-0.0225rem] md:text-[2.25rem] md:leading-[1.3] md:tracking-[-0.0225rem] focus:border-grayDark"
+      className="h-[71px] text-grayDark text-[1.25rem] xsm:text-[1.5rem] not-italic font-semibold leading-[1.1] tracking-[0.24px] placeholder:text-grayMiddle placeholder:text-[1.75rem] placeholder:not-italic placeholder:font-semibold placeholder:leading-[1.1] placeholder:tracking-[-0.28px] border-b-2 border-b-grayMiddle md:h-[87px] md:placeholder:text-[2.25rem] md:placeholder:leading-[1.3] md:placeholder:tracking-[-0.0225rem] md:text-[2.25rem] md:leading-[1.3] md:tracking-[-0.0225rem] focus:border-grayDark"
       {...register(label, { required })}
       {...{ placeholder, type }}
     />
