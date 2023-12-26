66a41253baa56385eff1fe40fe61edd7c6dbbdc4
DenisFeofanov <omail4me@gmail.com>
2023-12-26 21:49:44 +0500

    [fix] show footer description only for main

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,15 @@ export default function Footer() {
 
   return (
     <footer className="px-[15px] pb-[85px] flex flex-col items-center text-center text-grayDark text-[0.6875rem] font-normal leading-[1.5] md:text-[0.8125rem] md:px-0 md:p-0 md:bg-[#F5F5F5] md:pt-[30px] md:pb-[65px]">
-      <p className="hidden sm:block sm:px-[15px] sm:mx-auto sm:mb-[30px] sm:max-w-[640px] sm:text-center sm:text-blackText sm:text-[1.125rem] sm:font-normal sm:leading-[1.5] lg:[1.125rem]">
-        Спектакль стал результатом научно-театральной лаборатории «1000&nbsp;лет
-        вместе», исследующей отношение к смерти людей разных поколений,
-        социального положения и опыта. Премьера состоялась в 2019&nbsp;году в
-        рамках <NoWrap>5-й</NoWrap>&nbsp;Уральской индустриальной биеннале
-        современного искусства.
-      </p>
+      {currPathname === "/" && (
+        <p className="hidden sm:block sm:px-[15px] sm:mx-auto sm:mb-[30px] sm:max-w-[640px] sm:text-center sm:text-blackText sm:text-[1.125rem] sm:font-normal sm:leading-[1.5] lg:[1.125rem]">
+          Спектакль стал результатом научно-театральной лаборатории
+          «1000&nbsp;лет вместе», исследующей отношение к смерти людей разных
+          поколений, социального положения и опыта. Премьера состоялась в
+          2019&nbsp;году в рамках <NoWrap>5-й</NoWrap>&nbsp;Уральской
+          индустриальной биеннале современного искусства.
+        </p>
+      )}
 
       {currPathname !== "/feedback" && (
         <div className="mb-[30px]">
