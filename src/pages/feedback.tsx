23d6ddaed984b23b192ce82c36ad22362e22c163
DenisFeofanov <omail4me@gmail.com>
2023-12-27 13:10:45 +0500

    [feat] update buttons' hover visual

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -123,7 +123,7 @@ export default function Feedback() {
                   />
 
                   <button
-                    className={`mx-auto mt-[42px] leading-[normal] py-1 px-2 text-grayDark text-[1.125rem] font-semibold border-2 rounded-full border-transparent uppercase inline-flex gap-2 justify-center items-center before:content-["("] before:font-normal before:text-[1.33em] after:content-[")"] after:font-normal after:text-[1.33em] fine-pointer:hover:border-2 fine-pointer:hover:border-grayDark fine-pointer:hover:rounded-full fine-pointer:hover:before:opacity-0 fine-pointer:hover:after:opacity-0 active:bg-grayDark active:text-white active:border-2 active:border-grayDark active:rounded-full active:before:opacity-0 active:after:opacity-0 lg:text-[1.125rem]`}
+                    className={`inline-flex transition-all after:transition-all before:transition-all before:translate-x-[-3px] after:translate-x-[3px] mx-auto mt-[42px] leading-[normal] py-1 text-grayDark text-[1.125rem] font-semibold border-2 rounded-full border-transparent uppercase gap-2 justify-center items-center before:content-["("] before:font-normal before:text-[1.33em] after:content-[")"] after:font-normal after:text-[1.33em] fine-pointer:hover:border-2 fine-pointer:hover:border-grayDark fine-pointer:hover:rounded-full fine-pointer:hover:before:opacity-0 fine-pointer:hover:after:opacity-0 active:bg-grayDark active:text-white active:border-2 active:border-grayDark active:rounded-full active:before:opacity-0 active:after:opacity-0 lg:text-[1.125rem]`}
                     type="submit"
                   >
                     Отправить
