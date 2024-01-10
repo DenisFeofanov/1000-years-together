05d9b6453c3439ea8fa69981f46bf460e3429548
DenisFeofanov <omail4me@gmail.com>
2024-01-10 19:06:43 +0500

    [feat] add acts audio

--- src/shared/Act.ts
+++ src/shared/Act.ts
@@ -1,10 +1,14 @@
 import { Act } from "@/interfaces/Act";
-import audioSrc from "@/../public/stories/test.mp3";
+import prologueAudio from "@/audio/acts/01_prolog_short.mp3";
+import beginningAudio from "@/audio/acts/02_zavyazka_short.mp3";
+import middleAudio from "@/audio/acts/03_kulminatsiya_short.mp3";
+import endAudio from "@/audio/acts/04_razvyazka_short.mp3";
+import epilogueAudio from "@/audio/acts/05_epilog_short.mp3";
 
 export const ACTS: Act[] = [
-  { slug: "prologue", title: "Пролог", audioSrc },
-  { slug: "beginning", title: "Завязка", audioSrc },
-  { slug: "middle", title: "Кульминация", audioSrc },
-  { slug: "end", title: "Развязка", audioSrc },
-  { slug: "epilogue", title: "Эпилог", audioSrc },
+  { slug: "prologue", title: "Пролог", audioSrc: prologueAudio },
+  { slug: "beginning", title: "Завязка", audioSrc: beginningAudio },
+  { slug: "middle", title: "Кульминация", audioSrc: middleAudio },
+  { slug: "end", title: "Развязка", audioSrc: endAudio },
+  { slug: "epilogue", title: "Эпилог", audioSrc: epilogueAudio },
 ];
