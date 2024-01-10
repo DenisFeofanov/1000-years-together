c89cda06d8b97740c244291bea477bb377abc267
DenisFeofanov <omail4me@gmail.com>
2024-01-10 21:59:18 +0500

    [fix] incorrect symbols in filename for audio

--- src/shared/Stories.ts
+++ src/shared/Stories.ts
@@ -2,6 +2,7 @@ import { Story } from "@/interfaces/Story";
 
 // get all audiofiles from the folder
 const allEntries = require.context("@/audio/stories/", false, /.mp3$/i);
+
 // remove duplicates because apparently require.context always creates them
 const audioFiles = [...new Set(allEntries.keys().map(allEntries))] as string[];
 export const stories: Story[] = audioFiles.map(file => ({
